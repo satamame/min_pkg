5c14c7bf5d49d04ce52e8ca7d0d94487515a2d76
satamame <satax@nifty.com>
2020-12-13 18:08:34 +0900

    First commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="min_pkg",
+    version="0.1",
+    author="satamame",
+    author_email="satax@nifty.com",
+    description="Minimum package sample",
+    url="https://github.com/satamame/",
+    packages=['min_pkg'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
